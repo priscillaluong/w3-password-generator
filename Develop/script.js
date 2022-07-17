48a40b5efc8f9f1bbd81338e7d9a93b84f0d367f
P Luong <p-luong@live.co.uk>
2022-07-17 17:09:08 +0100

    updated generatePassword funct

--- Develop/script.js
+++ Develop/script.js
@@ -27,18 +27,57 @@ function writePassword() {
   //check is pw length is between 8 and 129 chars
   while (pwLength < 8 || pwLength > 130) {
     pwLength = +window.prompt("Password must be at least 8 characters and no more than 128 characters. Please enter length of password: ");
+    return pwLength;
   };
-  //console.log(typeof pwLength);
+  //console.log(pwLength);
   hasNumbers = window.confirm("Does the password include numbers?");
   hasLowercase = window.confirm("Does the password include lowercase letters?");
   hasUppercase = window.confirm("Does the password include uppercase letters?");
   hasSpecialChars = window.confirm("Does the password include special characters?");
   
+  //generatePassword(hasNumbers, hasLowercase, hasUppercase, hasSpecialChars, pwLength)
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(number, lower, upper, special, pwLength) {
+  // TODO: Put password code here
+  // 1. Initialise pw variable
+  // 2. Filter out user selection
+  // 3. Loop over length, call generator functions
+  // 4. Add final password to password variable and return
+  var generatedPassword = "";
+  const typesCount = number + lower + upper + special;
+  //console.log(typesCount);
+  //.filter method is used to filter through anything that is false
+  const typesArr = [{number}, {lower}, {upper}, {special}].filter
+  (
+    item => Object.values(item)[0]
+  );
+  //console.log(typesArr);
+  //if user values return false, we generate/return nothing
+  if(typesCount === 0) {
+    return "";
+  }
+
+  for(i = 0; i < pwLength; i += typesCount) {
+    //.foreach method calls a function for each element in an array
+    typesArr.forEach(type => {
+      const funcName = Object.keys[type][0];
+      //console.log(funcName);
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  console.log(generatedPassword);
+
+  const finalPassword = generatedPassword.slice(0,pwLength);
+  return finalPassword;
 }
 
 randomFunc = {
@@ -82,16 +121,6 @@ function getRandomSpecialChars(x) {
 } 
 // Below returns ONE random special character
 //var randomSpecialChars = specialChars.charAt(Math.floor(Math.random() * specialChars.length));
-
-function generatePassword() {
-  // TODO: Put password code here
-
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 
 //When button is clicked, a series of prompts pop up for password criteria
 
